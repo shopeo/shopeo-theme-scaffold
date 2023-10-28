090eb4f0d53835fe941622e3c462bc36c6cdc3b0
jabber <2898117012@qq.com>
2023-10-29 01:37:43 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	content: [],
+	theme: {
+		extend: {},
+	},
+	corePlugins: {
+		preflight: false,
+	},
+	experimental: {
+		optimizeUniversalDefaults: true,
+	},
+	plugins: [],
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,3 @@
 let mix = require('laravel-mix');
 
-mix.js('src/assets/js/app.js', 'assets/js').sass('src/assets/scss/style.scss', '', [], [require('postcss-import'), require('autoprefixer'),]).sass('src/assets/scss/style-rtl.scss', '', [], [require('postcss-import'), require('rtlcss'), require('autoprefixer'),]);
+mix.js('src/assets/js/app.js', 'assets/js').sass('src/assets/scss/style.scss', '', [], [require('postcss-import'), require('tailwindcss'), require('autoprefixer'),]).sass('src/assets/scss/style-rtl.scss', '', [], [require('postcss-import'), require('tailwindcss'), require('rtlcss'), require('autoprefixer'),]);
